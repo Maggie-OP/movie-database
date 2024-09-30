ea5eb840bd40ea2ddf4c6e6f0e1dc79a06318f9c
Margarit Shakaryan <margarit@Margarit-Shakaryan-MBP.local>
2024-09-30 20:16:07 +0400

    fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,29 +11,6 @@ const config: Config = {
       fontFamily: {
         sans: ["Source Sans Pro", "Arial", "sans-serif"],
       },
-      colors: {
-        "blue-1000": "#032541",
-        "blue-100": "#01B4E4",
-        "gray-100": "#e3e3e3",
-        "gray-900": "#081c22",
-      },
-      fontSize: {
-        "text-sup": ["4px", "4px"],
-        base: ["16px", "18px"],
-        xl: ["18px", "24px"],
-        xxl: ["20px", "20px"],
-        "heading-3": ["20px", "20px"],
-        "heading-1": ["26px", "32px"],
-        "footer-text": ["18px", "24px"],
-        "pagination-btn": ["24px", "28px"],
-        m: ["14px", "14px"],
-      },
-      boxShadow: {
-        shadow: "0 2px 8px 0 rgb(0,0,0,0.1)",
-      },
-      gridTemplateColumns: {
-        "movie-card": "repeat(auto-fit, minmax(176px, auto))",
-      },
     },
   },
   plugins: [],
