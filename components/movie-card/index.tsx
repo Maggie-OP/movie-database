8c86803dac018d3d6c9beb6d6a1338141fede13c
Margarit Shakaryan <margarit@Margarit-Shakaryan-MBP.local>
2024-10-01 16:53:56 +0400

    deleted tailwind classes from moviecard

--- components/movie-card/index.tsx
+++ components/movie-card/index.tsx
@@ -34,13 +34,10 @@ export default function MovieCard({ movie }: MovieCardProps) {
             alt={movie.title}
             width={100}
             height={100}
-            className="w-full h-full object-cover border-b border-b-gray-100"
           />
         </CardMediaLink>
-        <CardOptions className="absolute w-[26px] h-[26px] rounded-full bg-gray-400 top-4 right-4 hover:bg-blue-100"></CardOptions>
-        <MovieRating className="absolute left-3 top-full -translate-y-1/2 w-[34px] h-[34px] rounded-full bg-gray-900 flex items-center justify-center">
-          {(movie.vote_average * 10).toFixed(0)}
-        </MovieRating>
+        <CardOptions></CardOptions>
+        <MovieRating>{(movie.vote_average * 10).toFixed(0)}</MovieRating>
       </CardMedia>
       <CardContent>
         <CardContentTitle>
